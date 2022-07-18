a7ac2dd5d7864ecb8bb5147f5caef21c7cd5de8c
Sanna Davide <davide.sanna@euris.it>
2022-07-18 11:49:06 +0200

    Commit ISP principle part2

--- src/main/java/it/euris/solid/lsp/sample2/good/CardService.java
+++ src/main/java/it/euris/solid/lsp/sample2/good/CardService.java
@@ -2,7 +2,7 @@ package it.euris.solid.lsp.sample2.good;
 
 public class CardService {
 
-    public boolean isBetterThan(PlayingCard firstCard, PlayingCard secondCard) {
+    public boolean isBetterThan(RankingCard firstCard, RankingCard secondCard) {
         return firstCard.getRank() > secondCard.getRank();
     }
 
--- src/main/java/it/euris/solid/lsp/sample2/good/Main.java
+++ src/main/java/it/euris/solid/lsp/sample2/good/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PlayingCard card1 = new PlayingCard();
+        RankingCard card1 = new RankingCard();
         CardService cardService = new CardService();
 
         card1.setRank(1);
-        PlayingCard card2 = new PlayingCard();
+        RankingCard card2 = new RankingCard();
         card2.setRank(10);
 
         Joker joker = new Joker();
-        System.out.println(cardService.isBetterThan(joker, card2));
+//        System.out.println(cardService.isBetterThan(joker, card2));
         System.out.println(cardService.isBetterThan(card1, card2));
 
     }
