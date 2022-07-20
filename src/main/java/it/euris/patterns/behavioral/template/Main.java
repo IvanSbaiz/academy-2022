22c9c6e6781714d5ca8df0bfca7194b6c6aac8df
Sanna Davide <davide.sanna@euris.it>
2022-07-20 12:08:17 +0200

    pattern Template commit

---
+++ src/main/java/it/euris/patterns/behavioral/template/Main.java
@@ -0,0 +1,13 @@
+package it.euris.patterns.behavioral.template;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+        houseType.buildHouse();
+        System.out.println("************");
+        houseType = new GlassHouse();
+        houseType.buildHouse();
+    }
+
+}
